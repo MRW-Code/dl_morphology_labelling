f0412b29977d959dfeaba6f7e6aba63e9d4461b2
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-03-03 14:14:37 +0000

    new test/val shuffle

--- training/data_processing/model_training_script.py
+++ training/data_processing/model_training_script.py
@@ -12,10 +12,32 @@ crystals = DataBlock(
     splitter= RandomSplitter(valid_pct=0.1, seed=0),
     get_y =parent_label)
 
-dls = crystals.dataloaders(path / 'data/training_images/Images', bs=8)
+# tfms = aug_transforms(pad_mode='zeros', mult=2, min_scale=0.5)
+tfms = aug_transforms(mult=10, do_flip=True,
+    flip_vert=False,
+    max_rotate=10.0,
+    min_zoom=1.0,
+    max_zoom=1.1,
+    max_lighting=0.2,
+    max_warp=0.2,
+    p_affine=0.75,
+    p_lighting=0.75,
+    xtra_tfms=None,
+    size=None,
+    mode='bilinear',
+    pad_mode='zeros',
+    align_corners=True,
+    batch=False,
+    min_scale=1.0)
+
+
+dls = crystals.dataloaders(path / 'data/training_images/Images', bs=32, batch_tfms=tfms)
 
 learn = cnn_learner(dls, resnet50, metrics=[error_rate, accuracy])
-learn.fine_tune(3)
+learn.fine_tune(20, cbs=[SaveModelCallback(fname='./best_cbs_100'),
+                                  ReduceLROnPlateau(monitor='valid_loss',
+                                                    min_delta=0.1,
+                                                    patience=2)])
 
 learn.export('./trained_model.pkl')
 
---
+++ val_test_change.py
@@ -0,0 +1,50 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+from fastai.vision.all import *
+import pandas as pd
+path = Path(os.path.dirname('./'))
+Path.BASE_PATH = path
+
+
+def get_train_images(path):
+    images = get_image_files(path)
+    labels = [x.parent.name for x in images]
+    df = pd.DataFrame({'fname' : images,
+                       'label' : labels})
+    df['is_valid'] = 0
+    return df
+
+def get_val_labels(path):
+    ref = pd.read_csv('./data/summer_hts_data_matt.csv', index_col=0)
+    fnames = []
+    labels = []
+    for drug in ref.api:
+        parent_name = f'./data/water/water/{drug}'
+        parent_content = os.listdir(parent_name)
+        for name in parent_content:
+            fnames.append(f'{parent_name}/{name}')
+            labels.append(ref.eye_morphology[ref['api'] == drug].values[0])
+    df = pd.DataFrame({'fname' : fnames,
+                       'label' : labels})
+    df['is_valid'] = 1
+    return df
+
+train_df = get_train_images('./data/training_images/Images')
+val_df = get_val_labels('./data/water')
+model_df = pd.concat([train_df, val_df], axis=0)
+print(model_df.head(), model_df.shape)
+tfms = None
+dls = ImageDataLoaders.from_df(model_df,
+                               fn_col=0,
+                               label_col=1,
+                               valid_col=2,
+                               batch_tfms=tfms)
+
+learn = cnn_learner(dls, resnet50, metrics=[error_rate, accuracy, F1Score])
+learn.fine_tune(20, cbs=[SaveModelCallback(fname='./best_cbs_100'),
+                                  ReduceLROnPlateau(monitor='valid_loss',
+                                                    min_delta=0.1,
+                                                    patience=2)])
+learn.export('./trained_model.pkl')
+
+
